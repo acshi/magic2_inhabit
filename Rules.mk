b9d1d5adbbf84442a36f851ae3308b4fc24ed69d
Acshi Haggenmiller <acshikh@umich.edu>
2017-09-08 13:36:13 -0400

    upgraded magic2 to meson as well

--- Rules.mk
+++
@@ -1,2 +0,0 @@
-include $(REAL_ROOT_PATH)/magic2/Rules.mk
-include $(REAL_ROOT_PATH)/src/Rules.mk
--- src/Rules.mk
+++
@@ -1,3 +0,0 @@
-include $(REAL_ROOT_PATH)/src/lcmtypes/Rules.mk
-include $(REAL_ROOT_PATH)/src/tag_follower/Rules.mk
-include $(REAL_ROOT_PATH)/src/follow_viewer/Rules.mk
