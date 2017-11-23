bfd41670e0f997eeca7d0bd6aa7b523bb74558bd
Acshi Haggenmiller <acshikh@umich.edu>
2017-11-22 19:05:24 -0500

    added while loop so all messages would be handled not just one

--- src/acshi_common/lcm_handle_async.h
+++ src/acshi_common/lcm_handle_async.h
@@ -1,9 +1,6 @@
-#ifndef LCM_HANDLE_ASYNC_H
-#define LCM_HANDLE_ASYNC_H
+#pragma once
 
 #include <lcm/lcm.h>
 
 // Based on https://github.com/lcm-proj/lcm/blob/master/examples/c/listener-async.c#L59
 void lcm_handle_async(lcm_t *lcm);
-
-#endif
